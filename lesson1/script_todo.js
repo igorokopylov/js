a8c2ba2726239a5f820bca1384824bab3e360ce6
igorokopylov <66507754+igorokopylov@users.noreply.github.com>
2020-08-16 10:06:20 +0300

    Add files via upload

---
+++ lesson1/script_todo.js
@@ -0,0 +1,47 @@
+// Сделайте список задач (todos), нужно использовать типы значений - массив / объект / строку / число / булевое
+
+// Список задач:
+
+toDo = [{
+    task: "Beetroot IT course",
+    firstDate: "01.06.2020",
+    lastDate: "30.09.2020",
+    dayOfWeek: ["wednesday", "friday", "sunday"],
+    price: 14200,
+    doingStatus: true
+}, {
+    task: "Beetroot English course",
+    firstDate: "01.06.2020",
+    lastDate: "30.09.2020",
+    dayOfWeek: ["monday"],
+    price: 14200,
+    doingStatus: true
+}, {
+    task: "Capital Letter English course",
+    firstDate: "01.09.2020",
+    lastDate: "31.12.2020",
+    dayOfWeek: ["tuesday", "thursday"],
+    price: 560,
+    doingStatus: false
+}, {
+    task: "Remiz Driver School",
+    firstDate: "01.06.2020",
+    lastDate: "30.09.2020",
+    dayOfWeek: ["tuesday", "thursday"],
+    price: 7000,
+    doingStatus: true
+}, {
+    task: "Dirom Sport JYM",
+    firstDate: "01.11.2019",
+    lastDate: "31.12.2020",
+    dayOfWeek: ["monday", "wednesday", "friday"],
+    price: 670,
+    doingStatus: true
+}, {
+    task: "Social Dance Club Salsa Course",
+    firstDate: "01.11.2019",
+    lastDate: "31.12.2020",
+    dayOfWeek: ["tuesday", "thursday"],
+    price: 420,
+    doingStatus: true
+}]
