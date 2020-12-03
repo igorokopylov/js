86b22696f327086428cfbbb5155296bb6dbb8734
igorokopylov <66507754+igorokopylov@users.noreply.github.com>
2020-12-03 09:56:09 +0200

    Update script_3_1.js

--- lesson3/script_3_1.js
+++ lesson3/script_3_1.js
@@ -1,11 +1,15 @@
 // Подсчитать сумму всех чисел в заданном пользователем диапазоне.
+
 console.log("ЗАДАЧА");
-let someNumber = prompt('Задача: Подсчитать сумму всех чисел в заданном пользователем диапазоне. Напишите число');
+console.log("Подсчитать сумму всех чисел в заданном пользователем диапазоне.");
+
+let someNumber = prompt('Напишите число');
 let together = 0;
 
 while (Number(someNumber) > 0) {
     together = together + Number(someNumber);
     someNumber--;
 }
+
 console.log("ОТВЕТ");
 console.log(together);
